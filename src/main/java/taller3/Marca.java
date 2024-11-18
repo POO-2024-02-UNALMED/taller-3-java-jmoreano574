3badfc022ef9d2d6fea5ab3d65f7435466eb4e19
jmoreano574 <jmoreano@unal.edu.co>
2024-11-18 05:18:09 -0500

    Create Marca.java

---
+++ src/main/java/taller3/Marca.java
@@ -0,0 +1,21 @@
+package taller3.televisores;
+
+public class Marca 
+{
+    private String nombre;
+
+    public Marca(String nombre)
+    {
+        this.set_Nombre(nombre);
+    }
+
+    public void set_nombre(String marca)
+    {
+        this.nombre=nombre;
+    }
+    public String get_nombre(String nombre)
+    {
+        return nombre;
+    }
+}
+     
